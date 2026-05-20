30012965ba4e63a09671d3ca44a07e8d55221e1f
luanhayalla2 <luanhayalla2@gmail.com>
2026-05-20 15:40:31 -0300

    fix: append src to sys.path in wsgi.py to allow Vercel to resolve settings

--- src/quizsite/wsgi.py
+++ src/quizsite/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add the 'src' directory to the python path
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 from django.core.wsgi import get_wsgi_application
 
